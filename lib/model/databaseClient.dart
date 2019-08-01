eba85ca57082bb0585bac5831f09b23ab704bfa2
lyabs243 <loicyabili@outlook.com>
2019-08-01 14:52:55 +0200

    create database and item table

---
+++ lib/model/databaseClient.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseClient{
+
+  Database _database;
+
+  Future<Database> get database async{
+    if(_database != null){
+      return _database;
+    }
+    else{
+
+    }
+  }
+
+  Future create() async{
+    Directory directory = await getApplicationDocumentsDirectory();
+    String database_directory = join(directory.path,'database.db');
+    var bdd = await openDatabase(
+        database_directory,
+        version: 1,
+        onCreate: _onCreate,
+    );
+
+    return bdd;
+  }
+
+  Future _onCreate(Database db,int version) async{
+    await db.execute(
+    '''
+      CREATE TABLE item(
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL
+      )
+    ''');
+  }
+
+}
